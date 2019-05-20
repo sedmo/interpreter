d4d560474a998e60e006ed27649dd0bf1d16b5aa
Stephan <stephanedmonson@gmail.com>
2019-05-19 21:14:04 -0400

    Updated parser funct

--- interpreter.py
+++ interpreter.py
@@ -34,65 +34,20 @@ def lex(text):
                 "Error during tokenization process! Unrecognized character: '" + text[i] + "'.")
   return d
 
-class Parser:
-
-    def __init__(self, tokens, stop_at):
-        self.tokens = tokens
-        self.stop_at = stop_at
-
-    def next_expression(self, prev):
-        self.fail_if_at_end(";")
-        typ, value = self.tokens.next
-        if typ in self.stop_at:
-            return prev
-        self.tokens.move_next()
-        if typ in ("NonZeroDigit", "Digit", "Letter") and prev is None:
-            return self.next_expression((typ, value))
-        elif typ == "Operator":
-            nxt = self.next_expression(None)
-            return self.next_expression(("Operator", value, prev, nxt))
-        elif typ == "(":
-            args = self.multiple_expressions(",", ")")
-            return self.next_expression(("Exp", prev, args))
-        elif typ == "=":
-            if prev[0] != "Identifier":
-                raise Exception(
-                    "You can't assign to anything except an identifier.")
-            nxt = self.next_expression(None)
-            return self.next_expression(("Assignment", prev, nxt))
-        else:
-            raise Exception("Unexpected token: " + str((typ, value)))
-
-    def multiple_expressions(self, sep, end):
-        ret = []
-        self.fail_if_at_end(end)
-        typ = self.tokens.next[0]
-        if typ == end:
-            self.tokens.move_next()
-        else:
-            arg_parser = Parser(self.tokens, (sep, end))
-            while typ != end:
-                p = arg_parser.next_expression(None)
-                if p is not None:
-                    ret.append(p)
-                typ = self.tokens.next[0]
-                self.tokens.move_next()
-                self.fail_if_at_end(end)
-        return ret
-
-    def fail_if_at_end(self, expected):
-        if self.tokens.next is None:
-            raise Exception("Hit end of file - expected '%s'." % expected)
-
-
-def parse(tokens):
-    # determine if it is an assignment
-    parser = Parser(tokens, ";")
-    while parser.tokens.next is not None:
-        p = parser.next_expression(None)
-        if p is not None:
-            yield p
-        parser.tokens.move_next()
+def parser(tokens):
+  # here we will add errors
+  if "=" not in [x[0] for x in tokens] or "identifier" not in [x[0] for x in tokens]:
+    raise Exception("Uninitialized variable")
+  types = [x[0] for x in tokens]
+  dict = {}
+  for t in types:
+    if t in dict:
+      dict[t] += 1
+    else:
+      dict[t] = 1
+  if dict["identifier"] != 1 or dict["="] < 1 or dict[";"] != 1:
+    raise Exception("SyntaxErrorWarning")
+  varTable = {}
 
 
 def main():
